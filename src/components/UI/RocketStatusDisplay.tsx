e6698170999fb480c98cbf52b34895c70cf4d3d6
Tibz-Dankan <tibesigwadankan@gmail.com>
2025-02-17 13:49:02 +0300

    feat: use the accelerator to control speed of the rocket

--- src/components/UI/RocketStatusDisplay.tsx
+++ src/components/UI/RocketStatusDisplay.tsx
@@ -15,20 +15,15 @@ export const RocketStatusDisplay = () => {
     const updateStatus = (data: typeof rocketStatus) => setRocketStatus(data);
 
     EventBus.on("rocketStatus", updateStatus);
-    // EventBus.on("rocketStatus", (data: typeof rocketStatus) => {
-    //   setRocketStatus(() => data);
-    // });
 
     return () => {
-      //   EventBus.off("rocketStatus");
       EventBus.off("rocketStatus", updateStatus);
     };
   }, []);
 
   return (
     <div className="text-gray-100 flex flex-col justify-center gap-4">
-      {/* <RocketSpeedometer speed={rocketStatus.speed} maxSpeed={20000} /> */}
-      <RocketSpeedometer speed={rocketStatus.speed} maxSpeed={200} />
+      <RocketSpeedometer speed={rocketStatus.speed} maxSpeed={20000} />
       <RocketSpeedAltitudeTable
         altitude={rocketStatus.altitude}
         speed={rocketStatus.speed}
