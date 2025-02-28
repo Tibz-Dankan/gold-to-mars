342096af4ae8ef5b1dee85091ea927c358b973d0
Tibz-Dankan <tibesigwadankan@gmail.com>
2025-02-28 22:04:13 +0300

    fix: properly capture and display rocket altitude

--- src/components/UI/RocketStatusDisplay.tsx
+++ src/components/UI/RocketStatusDisplay.tsx
@@ -4,33 +4,50 @@ import { RocketSpeedAltitudeTable } from "./RocketSpeedAltitudeTable";
 import { RocketSpeedometer } from "./RocketSpeedometer";
 
 export const RocketStatusDisplay = () => {
-  const [rocketStatus, setRocketStatus] = useState({
-    speed: 0,
-    altitude: 0,
-    planet: "Earth",
-  });
   const [rocketSpeed, setRocketSpeed] = useState({ speed: 0 });
+  const [rocketLocation, setRocketLocation] = useState({
+    location: "",
+    isApproachingEarth: false,
+    isApproachingMars: false,
+    distanceToEarthKm: 0,
+    distanceToMarsKm: 0,
+    distanceFromEarthToMarsKm: 0,
+  });
+
+  const isNearEarth: boolean =
+    rocketLocation.distanceToEarthKm <= rocketLocation.distanceToMarsKm;
+
+  const isEarth: boolean = rocketLocation.location.toUpperCase() === "EARTH";
+  const isMars: boolean = rocketLocation.location.toUpperCase() === "MARS";
+
+  const isEarthOrMars = isEarth || isMars;
+
+  const altitude = isNearEarth
+    ? isEarthOrMars
+      ? rocketLocation.distanceToEarthKm
+      : rocketLocation.distanceToEarthKm * 100
+    : isEarthOrMars
+    ? rocketLocation.distanceToMarsKm
+    : rocketLocation.distanceToMarsKm * 100;
 
   useEffect(() => {
-    const updateStatus = (data: typeof rocketStatus) => setRocketStatus(data);
     const updateSpeed = (speed: typeof rocketSpeed) => setRocketSpeed(speed);
+    const updateLocation = (data: typeof rocketLocation) =>
+      setRocketLocation(data);
 
-    EventBus.on("rocketStatus", updateStatus);
     EventBus.on("rocketSpeed", updateSpeed);
+    EventBus.on("rocketLocation", updateLocation);
 
     return () => {
-      EventBus.off("rocketStatus", updateStatus);
       EventBus.off("rocketSpeed", updateSpeed);
+      EventBus.off("rocketLocation", updateLocation);
     };
   }, []);
 
   return (
     <div className="text-gray-100 flex justify-center gap-4 pt-1">
       <RocketSpeedometer speed={rocketSpeed.speed} maxSpeed={20000} />
-      <RocketSpeedAltitudeTable
-        altitude={rocketStatus.altitude}
-        speed={rocketSpeed.speed}
-      />
+      <RocketSpeedAltitudeTable altitude={altitude} speed={rocketSpeed.speed} />
     </div>
   );
 };
