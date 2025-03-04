4725c94647cac923ba73a84d9af786ed6fe2aa87
Tibz-Dankan <tibesigwadankan@gmail.com>
2025-03-04 15:08:47 +0300

    clean: remove unnecessary comments

--- src/components/UI/StatisticsBarCharts.tsx
+++ src/components/UI/StatisticsBarCharts.tsx
@@ -5,16 +5,16 @@ import {
   XAxis,
   YAxis,
   Tooltip,
-  ResponsiveContainer,
+  // ResponsiveContainer,
 } from "recharts";
 import { useStatisticsStore } from "../../store/statistics";
 
 export const StatsQuantityBarChart: React.FC = () => {
-  const earthGoldQuantity = useStatisticsStore(
-    (state) => state.statistics.earthGoldQuantity
+  const earthGoldQuantity = useStatisticsStore((state) =>
+    parseInt(state.statistics.earthGoldQuantity.toFixed(0))
   );
-  const marsGoldQuantity = useStatisticsStore(
-    (state) => state.statistics.marsGoldQuantity
+  const marsGoldQuantity = useStatisticsStore((state) =>
+    parseInt(state.statistics.marsGoldQuantity.toFixed(0))
   );
 
   const quantityData = [
@@ -23,26 +23,30 @@ export const StatsQuantityBarChart: React.FC = () => {
   ];
 
   return (
-    <ResponsiveContainer width={200} height={200}>
-      <BarChart data={quantityData} barCategoryGap={20}>
-        <XAxis
-          dataKey="name"
-          stroke="#868e96"
-          tick={{ fill: "#ced4da", fontSize: 12 }}
-          angle={-45}
-        />
-        <YAxis stroke="#868e96" tick={{ fill: "#ced4da", fontSize: 12 }} />
-        <Tooltip contentStyle={{ fontSize: 12 }} />
-        <Bar dataKey="earthGold" fill="#1E90FF" name="Earth Gold" />
-        <Bar dataKey="marsGold" fill="#B7410E" name="Mars Gold" />
-      </BarChart>
-    </ResponsiveContainer>
+    // <ResponsiveContainer width={200} height={200}>
+    <BarChart width={200} height={200} data={quantityData} barCategoryGap={20}>
+      <XAxis
+        dataKey="name"
+        stroke="#868e96"
+        tick={{ fill: "#ced4da", fontSize: 12 }}
+        angle={-45}
+      />
+      <YAxis stroke="#868e96" tick={{ fill: "#ced4da", fontSize: 12 }} />
+      <Tooltip contentStyle={{ fontSize: 12 }} />
+      <Bar dataKey="earthGold" fill="#1E90FF" name="Earth Gold" />
+      <Bar dataKey="marsGold" fill="#B7410E" name="Mars Gold" />
+    </BarChart>
+    // </ResponsiveContainer>
   );
 };
 
 export const StatsValueBarChart: React.FC = () => {
-  const goldValue = useStatisticsStore((state) => state.statistics.goldValue);
-  const btcValue = useStatisticsStore((state) => state.statistics.btcValue);
+  const goldValue = useStatisticsStore((state) =>
+    parseInt(state.statistics.goldValue.toFixed(0))
+  );
+  const btcValue = useStatisticsStore((state) =>
+    parseInt(state.statistics.btcValue.toFixed(0))
+  );
 
   const valueData = [
     { name: "Gold Value", goldValue },
@@ -50,19 +54,19 @@ export const StatsValueBarChart: React.FC = () => {
   ];
 
   return (
-    <ResponsiveContainer width={200} height={200}>
-      <BarChart data={valueData} barCategoryGap={20}>
-        <XAxis
-          dataKey="name"
-          stroke="#868e96"
-          tick={{ fill: "#ced4da", fontSize: 12 }}
-          angle={-45}
-        />
-        <YAxis stroke="#868e96" tick={{ fill: "#ced4da", fontSize: 12 }} />
-        <Tooltip contentStyle={{ fontSize: 12 }} />
-        <Bar dataKey="goldValue" fill="#FFD700" name="Gold Value" />
-        <Bar dataKey="btcValue" fill="#F7931A" name="BTC Value" />
-      </BarChart>
-    </ResponsiveContainer>
+    // <ResponsiveContainer width={200} height={200}>
+    <BarChart width={200} height={200} data={valueData} barCategoryGap={20}>
+      <XAxis
+        dataKey="name"
+        stroke="#868e96"
+        tick={{ fill: "#ced4da", fontSize: 12 }}
+        angle={-45}
+      />
+      <YAxis stroke="#868e96" tick={{ fill: "#ced4da", fontSize: 12 }} />
+      <Tooltip contentStyle={{ fontSize: 12 }} />
+      <Bar dataKey="goldValue" fill="#FFD700" name="Gold Value" />
+      <Bar dataKey="btcValue" fill="#F7931A" name="BTC Value" />
+    </BarChart>
+    // </ResponsiveContainer>
   );
 };
